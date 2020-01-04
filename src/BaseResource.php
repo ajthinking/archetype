711717a15f2be02e022edb38cdf268c44e92b187
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-04 14:25:27 +0100

    Cleanup delegation

--- src/BaseResource.php
+++ src/BaseResource.php
@@ -4,12 +4,40 @@ namespace Ajthinking\PHPFileManipulator;
 
 use Ajthinking\PHPFileManipulator\PHPFile;
 use BadMethodCallException;
+use Illuminate\Support\Str;
 
 abstract class BaseResource
 {
     public function __construct(PHPFile $file)
     {
         $this->file = $file;      
+    }
+
+    public function getResourceName()
+    {
+        return Str::replaceLast(
+            'Resource', '', collect(explode('\\', static::class))->last()
+        );
+    }
+
+    public function __call($method, $args)
+    {
+        $resource = $this->getResourceName();
+
+        // exact matches are getters/setters
+        if(preg_match("/^$resource\$/i", $method)) {
+            return $args ? $this->set(...$args) : $this->get();
+        }
+        // adders
+        if(preg_match("/^add$resource\$/i", $method)) {
+            return $this->add(...$args);
+        }
+        // removers
+        if(preg_match("/^remove$resource\$/i", $method)) {
+            return $this->remove(...$args);
+        }        
+
+        throw new BadMethodCallException("Resource " . static::class . " could not resolve method " . $method);
     }
 
     const NOT_IMPLEMENTED = 'Method not implemented for this resource';
--- src/Traits/DelegatesAPICalls.php
+++ src/Traits/DelegatesAPICalls.php
@@ -3,75 +3,63 @@
 namespace Ajthinking\PHPFileManipulator\Traits;
 use Illuminate\Support\Str;
 use BadMethodCallException;
-use Ajthinking\PHPFileManipulator\BaseResource;
-use Ajthinking\PHPFileManipulator\BaseSnippet;
 
 trait DelegatesAPICalls
 {
+    /**
+     * Any call to the file that cant be found as a method on $this are catched here
+     * We will try to find a handler to delegate to
+     *
+     * @param [string] $method
+     * @param [array] $args
+     * @return mixed
+     */
     public function __call($method, $args) {
-        // Find handler instance (resource, snippet or this)
         $handler = $this->getHandler($method);
-        // setter, getter, adder, remover or other ?
-        $requestType = $this->getRequestType($method, $args);
-        // dispatch method call to handler
-        return $handler->$requestType(...$args);
+        return $handler->$method(...$args);
     }
 
-    private function getRequestType($method, $args)
-    {
-        foreach($this->resources() as $resource) {
-            // exact matches are getters/setters
-            if(preg_match("/^$resource\$/i", $method)) {
-                return $args ? 'set' : 'get';
-            }
-            // adders
-            if(preg_match("/^add$resource\$/i", $method)) {
-                return 'add';
-            }
-            // removers
-            if(preg_match("/^remove$resource\$/i", $method)) {
-                return 'remove';
-            }            
-        }
-
-        if($this->getHandler($method) instanceof BaseSnippet) return 'renderAST';
-
-        throw new BadMethodCallException("Could not resolve method type");
+    /**
+     * Find a handler to delegate to
+     *
+     * @param [string] $method
+     * @return [Resource|Template|QueryBuilder]
+     */
+    private function getHandler($method) {
+        $handler = collect([
+            $this->getResourceHandler($method),
+            $this->getTemplateHandler($method),
+            $this->getQueryBuilderHandler($method),
+        ])->filter()->first();
+    
+        if($handler) return $handler;
+    
+        throw new BadMethodCallException("Could not find a handler for method $method");
     }
 
-    private function getHandler($method)
+    private function getResourceHandler($method)
     {
-        // Resource?
         $resource = $this->resources()->filter(function($resource) use($method) {
             return preg_match("/^$resource\$/i", $method)
                 || preg_match("/^add$resource\$/i", $method)
                 || preg_match("/^remove$resource\$/i", $method);
         })->first();
-        
+
         if($resource) {
             $resourceClass = "Ajthinking\PHPFileManipulator\Resources\\" . Str::studly($resource) . "Resource";
             return new $resourceClass($this);
         };
 
-        // Snippet?
-        $snippet = collect(isset($this->snippets) ? $this->snippets : [])->filter(function($snippet) use($method) {
-            return preg_match("/^$snippet\$/i", $method);
-        })->first();
-        
-        if($snippet) {
-            $snippetClass = "Ajthinking\PHPFileManipulator\Snippets\\" . Str::studly($snippet) . "Snippet";
-            return new $snippetClass($this);
-        };
-
-        // Defaults to $this
-        return $this;
+        return false; // this method could not be handled by a resource
     }
-
-    private function callOnSelf($method, $args = [])
+    
+    private function getTemplateHandler($method)
     {
-        $methods = get_class_methods($this);
-        if(collect($methods)->contains($method)) return $this->$method($args);
-
-        throw new BadMethodCallException("$method: No such method!");
+        return false;
     }
+
+    private function getQueryBuilderHandler($method)
+    {
+        return false;
+    }    
 }
