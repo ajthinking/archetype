453d01c240e9507d66f251a8653cb9a7c21df62a
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-08 12:42:21 +0100

    Added CanUseSnippets trait

--- src/PHPFile.php
+++ src/PHPFile.php
@@ -5,12 +5,14 @@ namespace Ajthinking\PHPFileManipulator;
 use Ajthinking\PHPFileManipulator\Traits\DelegatesAPICalls;
 use Ajthinking\PHPFileManipulator\Traits\HasIO;
 use Ajthinking\PHPFileManipulator\Traits\HasTemplates;
+use Ajthinking\PHPFileManipulator\Traits\CanUseSnippets;
 
 class PHPFile
 {
     use DelegatesAPICalls;
     use HasIO;
     use HasTemplates;
+    use CanUseSnippets;
 
     public function resources() {
         return collect([
