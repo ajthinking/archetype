b221fd953d119b766287580d0ee44bd5b7a96492
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-08 14:32:48 +0100

    Added some tests

--- tests/Unit/SnippetTest.php
+++ tests/Unit/SnippetTest.php
@@ -3,18 +3,39 @@
 namespace Ajthinking\PHPFileManipulator\Tests\Unit;
 
 use Ajthinking\PHPFileManipulator\Tests\TestCase;
+use PhpParser\Node\Stmt\ClassMethod;
 
 use LaravelFile;
 
 class SnippetTest extends TestCase
 {
     /** @test */
-    public function it_can_use_snippets_from_default()
+    public function it_can_load_class_methods_from_snippet_defaults()
     {
-        LaravelFile::snippet('mySnippet');
-
-        $this->assertTrue(
-            true
+        $this->assertInstanceOf(
+            ClassMethod::class,
+            LaravelFile::snippet('___HAS_MANY_METHOD___')
         );            
-    }   
+    }
+
+    /** @test */
+    public function it_can_replace_snippet_names()
+    {
+        $method = LaravelFile::snippet('___HAS_MANY_METHOD___', [
+            '___HAS_MANY_METHOD___' => 'guitars'
+        ]);
+
+        $this->assertEquals(
+            $this->laravelUserFile()->addClassMethods([$method])->classMethodNames(),
+            ['cars', 'guitars']
+        );            
+    }    
+    
+    /** @test */
+    public function it_cant_load_non_existing_snippets_from_defaults()
+    {
+        $this->assertNull(
+            LaravelFile::snippet('NoSUchSnippet')
+        );            
+    }    
 }
