b48154235b180555fdd1ba02a9b6307509f14e12
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-11 14:12:02 +0100

    Cleanup of CanUseSnippets

--- src/Traits/CanUseSnippets.php
+++ src/Traits/CanUseSnippets.php
@@ -2,63 +2,12 @@
 
 namespace Ajthinking\PHPFileManipulator\Traits;
 
-use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Property;
-use PhpParser\NodeFinder;
-use PhpParser\Node\Identifier;
-use PhpParser\Node\Name\FullyQualified;
-use PhpParser\Node\Name;
+use Ajthinking\PHPFileManipulator\Snippets\SnippetFile;
 
 trait CanUseSnippets
 {
     public function snippet($name, $replacementPairs = [])
     {
-        $node = $this->load(
-            'packages/Ajthinking/PHPFileManipulator/src/Snippets/defaults.php'
-        )->getNodeByName($name);
-
-        // REPLACE IDENTIFIERS
-        collect($replacementPairs)->each(function($replacementValue, $toBeReplaced) use($node) {
-            collect((new NodeFinder)->findInstanceOf(
-                $node, Identifier::class
-            ))->each(function($identifier) use($toBeReplaced, $replacementValue) {
-                if($identifier->name == $toBeReplaced) {
-                    $identifier->name = $replacementValue;
-                }
-            });            
-        });
-
-        // REPLACE NAMES
-        // collect($replacementPairs)->each(function($replacementValue, $toBeReplaced) use($node) {
-        //     collect((new NodeFinder)->findInstanceOf(
-        //         $node, Identifier::class
-        //     ))->each(function($identifier) use($toBeReplaced, $replacementValue) {
-        //         if($identifier->name == $toBeReplaced) {
-        //             $identifier->name = $replacementValue;
-        //         }
-        //     });            
-        // });
-        
-        // REPLACE COMMENTS
-
-        return $node;
-    }
-    
-    private function getNodeByName($name)
-    {
-        return collect([
-            $this->getMethodByName($name)
-            // add more findable types here
-        ])->filter()->first();
-    }
-
-    public function getMethodByName($requestedName)
-    {
-        return collect((new NodeFinder)->findInstanceOf(
-            $this->ast(),
-            ClassMethod::class
-        ))->filter(function($node) use($requestedName) {
-            return $node->name->name == $requestedName;
-        })->first();
+        return SnippetFile::$name($replacementPairs);
     }
 }
