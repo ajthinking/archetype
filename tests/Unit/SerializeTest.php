0401b45fde5a22894b8cd95cf9f56641470a4d02
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-11 14:09:08 +0100

    Added serialize test (for php-parser)

---
+++ tests/Unit/SerializeTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Ajthinking\PHPFileManipulator\Tests\Unit;
+
+use Ajthinking\PHPFileManipulator\Tests\TestCase;
+use PhpParser\ParserFactory;
+use PhpParser\JsonDecoder;
+
+class SerializeTest extends TestCase
+{
+    /** @test */
+    public function it_can_serialize_simple_files()
+    {
+        $code = '
+            <?php
+            
+            /** @param string $msg */
+            function printLine($msg) {
+                echo $msg, "\n";
+            }
+        ';
+        
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        
+        $ast = $parser->parse($code);
+        $text = json_encode($ast);
+        $stmts = (new JsonDecoder())->decode($text);
+
+        $this->assertTrue(
+            is_array($stmts)
+        );
+    }
+
+    /** @test */
+    public function it_can_serialize_complex_files()
+    {
+        $code = file_get_contents(
+            __DIR__ . '/../FileSamples/app/User.php'
+        );
+
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        
+        $ast = $parser->parse($code);
+        $text = json_encode($ast);
+        $stmts = (new JsonDecoder())->decode($text);
+
+        $this->assertTrue(
+            is_array($stmts)
+        );
+    }    
+}
