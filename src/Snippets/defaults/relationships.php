3295a25bdf957ee170bc833fc6216f474b110410
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-11 14:12:38 +0100

    creating a folder for templates to live :heart:

---
+++ src/Snippets/defaults/relationships.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Setup any FAKE names we need
+ */
+use Ajthinking\PHPFileManipulator\Support\FakeName; 
+use Ajthinking\PHPFileManipulator\Support\FakeName as ___TARGET_CLASS___;
+
+/**
+ * This is just a placeholder class where we can add our snippets
+ */
+class _ extends FakeName
+{
+    /**
+    * Get the associated ___TARGET_IN_DOC_BLOCK___
+    */
+    public function ___HAS_MANY_METHOD___()
+    {
+        return $this->hasMany(___TARGET_CLASS___::class);
+    }
+
+    /**
+    * Get the associated ___TARGET_IN_DOC_BLOCK___
+    */    
+    public function ___HAS_ONE_METHOD___()
+    {
+        return $this->hasOne(___TARGET_CLASS___::class);
+    }    
+
+    /**
+    * Get the associated ___TARGET_IN_DOC_BLOCK___
+    */
+    public function ___BELONGS_TO_METHOD___()
+    {
+        return $this->belongsTo(___TARGET_CLASS___::class);
+    } 
+
+    /**
+    * Get the associated ___TARGET_IN_DOC_BLOCK___
+    */    
+    public function ___BELONGS_TO_MANY_METHOD___()
+    {
+        return $this->belongsToMany(___TARGET_CLASS___::class);
+    }    
+}
