de86f99062d9c23fa1efc9791b1d776bbda9586b
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-12 00:13:54 +0100

    Cleanup

--- src/Traits/DelegatesAPICalls.php
+++ src/Traits/DelegatesAPICalls.php
@@ -4,36 +4,25 @@ namespace PHPFileManipulator\Traits;
 
 use BadMethodCallException;
 
-use PHPFileManipulator\Resolvers\TemplateResolver;
 use PHPFileManipulator\Resolvers\QueryBuilderResolver;
 
 trait DelegatesAPICalls
 {
     /**
-     * Time to delegate!
+     * Time to delegate! This class is just a gateway.
      */
     public function __call($method, $args) {
         $handler = $this->endpoints()->filter(function($endpoint) use($method, $args) {
             return (new $endpoint($this))->canHandle($method, $args);
         })->first();
 
-        if($handler) {
-            return (new $handler($this))->$method(...$args);
-        }
+        if($handler) return (new $handler($this))->$method(...$args);
+
 
         /** if querybuilder */
         $queryBuilder = QueryBuilderResolver::getHandler($this, $method); 
         if($queryBuilder) return $queryBuilder->$method(...$args); 
 
         throw new BadMethodCallException("Could not find a handler for method $method");
-    }
-    
-    public function getAPIMap()
-    {
-        return $this->endpoints()->map(function($endpoint) {
-            return collect($endpoint::aliases())->flatMap(function($alias) use($endpoint) {
-                return [$alias => $endpoint];
-            });
-        })->collapse();
-    }    
+    }   
 }
