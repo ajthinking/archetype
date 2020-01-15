75bad3382d68599054e3652f762188f4ad6cf74c
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-15 19:26:52 +0100

    Cleanup before new tests

--- tests/Unit/ReadWriteTest.php
+++ tests/Unit/ReadWriteTest.php
@@ -12,9 +12,7 @@ class PHPFileTest extends TestCase
     /** @test */
     public function it_can_load_php_files()
     {
-        $file = PHPFile::load(
-            $this->samplePath('index.php')
-        );
+        $file = PHPFile::load('public/index.php');
 
         $this->assertTrue(
             get_class($file) === 'PHPFileManipulator\PHPFile'
@@ -24,68 +22,10 @@ class PHPFileTest extends TestCase
     /** @test */
     public function it_can_also_load_laravel_specific_files()
     {
-        $file = LaravelFile::load(
-            $this->samplePath('app/User.php')
-        );
+        $file = LaravelFile::load('app/User.php');
 
         $this->assertInstanceOf(
             \PHPFileManipulator\LaravelFile::class, $file
         );
-    }
-
-
-    /** @discontinued-test */
-    public function it_has_path_getters()
-    {
-        $file = PHPFile::load(
-            $this->samplePath('app/User.php')
-        );
-
-        $this->assertTrue(
-            $file->relativePath() === 'tests/FileSamples/app/User.php'
-        );
-
-        $this->assertTrue(
-            $file->path() === base_path('tests/FileSamples/app/User.php')
-        );        
-    }    
-
-    /** @test */
-    public function it_can_write_to_disc()
-    {
-        // Save a copy
-        $this->userFile()->save(
-            $this->samplePath('.output/User.php')
-        );
-
-        // Read it
-        $copy = app('PHPFile')->load(
-            $this->samplePath('.output/User.php')
-        );
-
-        // Ensuring it is valid
-        $this->assertTrue(
-            get_class($copy) === 'PHPFileManipulator\PHPFile'
-        );
-
-        // NOTE: When pretty printing some of the array formatting may change
-        // For instance the $fillable array in Laravels default User class
-    }
-    
-    /** @depricated-test */
-    public function it_can_write_to_a_preview_folder()
-    {
-        // Save it
-        $this->userFile()->preview();
-
-        // Load it from the preview folder
-        $preview = PHPFile::load(
-            'storage/.preview/tests/FileSamples/app/User.php'
-        );
-
-        // It is valid
-        $this->assertTrue(
-            get_class($preview) === 'PHPFileManipulator\PHPFile'
-        );        
-    }   
+    }  
 }
