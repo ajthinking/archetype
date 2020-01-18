4b5a64bdfe58f26473037b02389090fb184b9149
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-18 14:00:03 +0100

    It can run stupid test

---
+++ tests/Unit/StupidTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PHPFileManipulator\Tests\Unit;
+
+use PHPFileManipulator\Tests\TestCase;
+
+/**
+ * @group stupid
+ */
+class StupidTest extends TestCase
+{
+    /** @test */
+    public function it_can_run_tests()
+    {
+        $this->assertTrue(true);
+    }  
+}
