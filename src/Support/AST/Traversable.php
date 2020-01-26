eb8c178cdebc18beb60f48abd96ece77c45128e6
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-26 13:20:09 +0100

    Added more UNTIL markers

--- src/Support/AST/Traversable.php
+++ src/Support/AST/Traversable.php
@@ -7,6 +7,8 @@ class Traversable
     const UNTIL = [
         'class' => \PhpParser\Node\Stmt\Class_::class,
         'method' => \PhpParser\Node\Stmt\ClassMethod::class,
+        'methodCall' => \PhpParser\Node\Expr\MethodCall::class,
         'staticCall' => \PhpParser\Node\Expr\StaticCall::class,
+        'closure' => \PhpParser\Node\Expr\Closure::class
     ];
 }
