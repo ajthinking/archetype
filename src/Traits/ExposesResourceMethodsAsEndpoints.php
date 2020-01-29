55d3dc8e455bf06237d6e7c5c97c06938fc9be47
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-29 13:28:38 +0100

    endpoint by resource convention

---
+++ src/Traits/ExposesResourceMethodsAsEndpoints.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace PHPFileManipulator\Traits;
+
+use BadMethodCallException;
+use ReflectionClass;
+use Illuminate\Support\Str;
+
+trait ExposesResourceMethodsAsEndpoints
+{
+    public function supportedEndpointMethods()
+    {
+        $reflection = new ReflectionClass(static::class);
+        return collect($reflection->getMethods())
+            ->filter(function($method) {
+                if(collect([
+                    '__call',
+                    '__construct',
+                    'canHandle',
+                    'getEndpoints',
+                    'getHandlerMethod',
+                    'supportedEndpointMethods',
+                    'aliases',
+                ])->contains($method->name)) return false;
+
+                if($method->isPublic()) return true;
+            })->values();        
+    }
+
+    public function getEndpoints()
+    {
+        $methods = $this->ownNonReservedPublicMethods();
+
+        return collect($methods)->map(function($verb) {
+            $resourceSignature = collect($this->aliases())->first();
+            $verbMap = [
+                'get' => $resourceSignature,
+                'set' => $resourceSignature,
+                'add' => 'add' . $resourceSignature,
+                'remove' => 'remove' . $resourceSignature,
+            ];
+            
+            return $verbMap[$verb];
+        });        
+
+        return $this->ownNonReservedPublicMethods();
+
+        $endpoints = $this->supportedEndpointMethods()
+            ->map(function($endpoint) {
+                $args = collect($endpoint->getParameters())->map(function($parameter) {
+                    return '$' . $parameter->getName();
+                })->join(', ');
+
+                return $endpoint->name . "($args)";
+        });
+
+        return $endpoints->toArray();
+    }  
+}
