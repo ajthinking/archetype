4515c5b8bf73848076d26f88619b884868fbf0fa
Anders Jurisoo <jurisoo@hotmail.com>
2020-01-29 13:27:47 +0100

    Placeholder description

--- src/Commands/ListAPICommand.php
+++ src/Commands/ListAPICommand.php
@@ -55,7 +55,7 @@ class ListAPICommand extends Command
                 return [
                     $endpoint,
                     '',
-                    $provider
+                    ''
                 ];
             })->toArray();
         });
