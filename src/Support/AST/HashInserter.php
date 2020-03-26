66163210d7589970ea5914bde21582e0bbf7a48a
Anders Jurisoo <jurisoo@hotmail.com>
2020-03-26 13:19:14 +0100

    The HashInserter contains its own usage logic

--- src/Support/AST/HashInserter.php
+++ src/Support/AST/HashInserter.php
@@ -7,10 +7,18 @@ use PhpParser\NodeFinder;
 use PhpParser\Node\Stmt\Use_;
 use PhpParser\NodeVisitorAbstract;
 use PhpParser\BuilderFactory;
+use PhpParser\NodeTraverser;
 
 class HashInserter extends NodeVisitorAbstract {
     public function leaveNode(Node $node) {
         $node->__object_hash = spl_object_hash($node);
         return $node;
+    }
+
+    public static function on($ast)
+    {
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor(new static);
+        return $traverser->traverse($ast);
     }
 }
