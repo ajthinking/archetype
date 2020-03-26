4859c685fff343d665a2e27f4698f4794c7a1937
Anders Jurisoo <jurisoo@hotmail.com>
2020-03-26 13:07:46 +0100

    Added shallow node finder

--- src/Support/AST/ShallowNodeFinder.php
+++ src/Support/AST/ShallowNodeFinder.php
@@ -4,4 +4,17 @@ namespace PHPFileManipulator\Support\AST;
 
 use PhpParser\NodeFinder;
 
-class ShallowNodeFinder extends NodeFinder {}
+class ShallowNodeFinder extends NodeFinder {
+
+    public function findInstanceOf($nodes, string $class) : array {
+        return collect($nodes)->filter(function ($node) use ($class) {
+            return $node instanceof $class;
+        })->toArray();
+    }
+
+    public function findFirstInstanceOf($nodes, string $class) {
+        return collect($nodes)->find(function ($node) use ($class) {
+            return $node instanceof $class;
+        });
+    }    
+}
