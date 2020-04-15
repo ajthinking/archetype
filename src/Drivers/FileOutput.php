e488f208413def504b070d35fa73c9cdf5802032
Anders Jurisoo <jurisoo@hotmail.com>
2020-04-15 20:01:12 +0200

    Draft on output->save()

--- src/Drivers/FileOutput.php
+++ src/Drivers/FileOutput.php
@@ -3,6 +3,8 @@
 namespace PHPFileManipulator\Drivers;
 
 use PHPFileManipulator\Drivers\OutputInterface;
+use Illuminate\Support\Str;
+use PHPFileManipulator\Support\PHPFileStorage;
 
 class FileOutput implements OutputInterface
 {
@@ -11,14 +13,43 @@ class FileOutput implements OutputInterface
     public $extension;
 
     public $relativeDir;
+
+    public $absoluteDir;
+
+    public $root;    
     
     public function save($path = null)
     {
-        //
+        $this->ensureDefaultRootExists();
+        $this->extractPathPropertiess();
     }
 
     public function debug($path = null)
     {
         //
+    }
+
+    protected function ensureDefaultRootExists()
+    {
+        $this->root = $this->root ?? config('php-file-manipulator')['roots']['output'];
+    }
+
+    protected function extractPathProperties($path)
+    {
+        preg_match('/(.*)\..*/', basename($path), $matches);
+        $this->filename = $path ? basename($path, '.php') : null;
+        
+        preg_match('/.*\.(.*)/', basename($path), $matches);
+        $this->extension = $matches[1] ?? null;
+        
+        $pathIsAbsolute = Str::startsWith($path, '/');
+
+        if($pathIsAbsolute) {
+            $this->absoluteDir = dirname($path);
+        } else {
+            $this->absoluteDir = dirname($this->root['root'] . "/" . $path);
+        }
+
+        $this->relativeDir = Str::replaceFirst($this->root['root'] . '/', '', $this->absoluteDir);
     }    
 }
