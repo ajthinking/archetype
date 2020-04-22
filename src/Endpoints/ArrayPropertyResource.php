c129b4ef4d3736e6a9bcf2d017eefb7aa447001e
Anders Jurisoo <jurisoo@hotmail.com>
2020-04-22 20:52:27 +0200

    Beautiful refactor using semi-completed ASTQueryBuilder

--- src/Endpoints/ArrayPropertyResource.php
+++ src/Endpoints/ArrayPropertyResource.php
@@ -9,6 +9,7 @@ use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Expr\ArrayItem;
 use PHPFileManipulator\Exceptions\NotImplementedYetException;
+use PHPParser\BuilderHelpers;
 
 abstract class ArrayPropertyResource extends ResourceEndpointProvider
 {
@@ -37,30 +38,14 @@ abstract class ArrayPropertyResource extends ResourceEndpointProvider
     }
     
     protected function setItems($requestedName, $items)
-    {        
-        $propertyGroups = (new NodeFinder)->findInstanceOf($this->ast(), Property::class);
-        if(!$propertyGroups) return null;
-
-        $ast = collect($propertyGroups)->map(function($propertyGroup) {
-            // Assume only one property per statement
-            return $propertyGroup->props[0];
-        })->filter(function($property) use($requestedName) {
-            return $property->name->name == $requestedName;
-        })->first();
-
-        if(!$ast) {
-            // Fix later
-            throw new NotImplementedYetException('Can not insert fillables if not allready present');
-        };
-
-        $ast->default = new Array_(
-            collect($items)->map(function($item) {
-                return new ArrayItem(
-                    new String_($item)
-                );
-            })->toArray()
-        );
-
+    {
+        $this->file->astQuery()
+            ->propertyProperty()
+            ->where('name->name', $requestedName)
+            ->default
+            ->replace(
+                BuilderHelpers::normalizeValue($items)
+            )->exit();
         
         return $this->file;
     }
