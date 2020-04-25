b0c765fbecfe69a4d8a3912a8cac0a5bdc9a5470
Anders Jurisoo <jurisoo@hotmail.com>
2020-04-25 18:48:20 +0200

    Added Property ENdpointProvider

---
+++ src/Endpoints/PHP/Property.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PHPFileManipulator\Endpoints\PHP;
+
+use PHPFileManipulator\Support\EndpointProvider;
+
+class Property extends EndpointProvider
+{
+    public function property($key, $value = null /* danger we might want to save null! */)
+    {
+        return "not implemented yet";
+    }
+}
