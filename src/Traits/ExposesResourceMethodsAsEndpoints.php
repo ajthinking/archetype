3e717c5e9a3c38494d125e7aeb6d05b78c6d5ed6
Anders Jurisoo <jurisoo@hotmail.com>
2020-04-25 17:17:18 +0200

    Simplified Array resource exposure

--- src/Traits/ExposesResourceMethodsAsEndpoints.php
+++ src/Traits/ExposesResourceMethodsAsEndpoints.php
@@ -8,28 +8,8 @@ use Illuminate\Support\Str;
 
 trait ExposesResourceMethodsAsEndpoints
 {
-    public function supportedEndpointMethods()
-    {
-        $reflection = new ReflectionClass(static::class);
-        return collect($reflection->getMethods())
-            ->filter(function($method) {
-                if(collect([
-                    '__call',
-                    '__construct',
-                    'canHandle',
-                    'getEndpoints',
-                    'getHandlerMethod',
-                    'supportedEndpointMethods',
-                    'aliases',
-                ])->contains($method->name)) return false;
-
-                if($method->isPublic()) return true;
-            })->values();        
-    }
-
     public function getEndpoints()
     {
-        
         $methods = $this->ownNonReservedPublicMethods();
 
         return collect($methods)->map(function($verb) {
