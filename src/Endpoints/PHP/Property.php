90da8230cb05f27956466e6fd107a4e0599bcea1
Anders Jurisoo <jurisoo@hotmail.com>
2020-04-29 09:44:41 +0200

    Basic PHP Property Endpoint

--- src/Endpoints/PHP/Property.php
+++ src/Endpoints/PHP/Property.php
@@ -6,15 +6,35 @@ use PHPFileManipulator\Endpoints\EndpointProvider;
 
 class Property extends EndpointProvider
 {
-    public function property($key, $value = null /* danger we might want to save null! */)
+    const NO_VALUE_PROVIDED = '___NO_VALUE_PROVIDED___';
+
+    /** NO_VALUE_PROVIDED is used to allow null insertion */
+    public function property($key, $value = self::NO_VALUE_PROVIDED)
     {
-        return "not implemented yet";
+        return $value == self::NO_VALUE_PROVIDED ? $this->get($key) : $this->set($key, $value);    
     }
 
-    public function setProperty($key, $value)
+    protected function get($key)
     {
-        "not implemented yet";
+        return $this->canUseReflection() ? $this->getWithReflection($key) : $this->getWithParser($key);
+    }
 
+    protected function set($key, $value /* danger we might want to save null! */)
+    {
         return $this->file;
+    }
+
+    protected function getWithReflection($name)
+    {
+        return $this->file->getReflection()->getDefaultProperties()[$name];
+    }
+
+    protected function getWithParser($name)
+    {
+        $result = $this->file->astQuery()
+            ->propertyProperty()
+            ->where('name->name', $requestedName);
+
+        dd($result);
     }
 }
