a4c777fb46b8dc8557e8bb348ff72077e5a7560d
Anders Jurisoo <jurisoo@hotmail.com>
2020-04-29 09:05:56 +0200

    Restructure test dir

--- tests/Unit/FilePathTest.php
+++ tests/Unit/FilePathTest.php
@@ -45,25 +45,6 @@ class FilePathTest extends FileTestCase
         );        
     }
     
-    // /** @test */
-    // public function a_file_has_an_output_path()
-    // {
-    //     // relative
-    //     $file = PHPFile::load('app/User.php');
-
-    //     $this->assertTrue(
-    //         Str::contains($file->outputPath(), '/.output/app/User.php')
-    //     );
-
-    //     // absolute
-    //     $path = base_path('app/User.php');
-    //     $file = PHPFile::load($path);
-    //     $this->assertTrue(
-    //         Str::contains($file->outputPath(), '/.output/app/User.php')
-    //     );        
-    // }
-    
-    /** @test */
     public function files_created_with_fromString_must_be_explicitly_named()
     {
         $file = PHPFile::fromString('<?php');
