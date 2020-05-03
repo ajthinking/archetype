16ad9ac45c457bc75c97108b43f9651693dbe5ce
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-03 08:35:25 +0200

    Node remover draft

---
+++ src/Support/AST/NodeRemover.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PHPFileManipulator\Support\AST;
+
+use PhpParser\Node;
+use PhpParser\NodeFinder;
+use PhpParser\Node\Stmt\Use_;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\BuilderFactory;
+use PhpParser\NodeTraverser;
+
+class NodeRemover extends NodeVisitorAbstract {
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    public function leaveNode(Node $node) {
+        return $node->__object_hash == $this->id ? NodeTraverser::REMOVE_NODE : $node;
+    }
+    
+    public static function remove($id, $ast)
+    {
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor(new static($id));
+        return $traverser->traverse($ast);
+    }    
+}
