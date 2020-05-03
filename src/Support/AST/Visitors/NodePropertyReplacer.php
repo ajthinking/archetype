d9dc8e22b3a6212e7b6a68b31a3db77219ac74f5
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-03 13:17:02 +0200

    Added NodePropertyReplacer

---
+++ src/Support/AST/Visitors/NodePropertyReplacer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PHPFileManipulator\Support\AST\Visitors;
+
+use PhpParser\Node;
+use PhpParser\NodeFinder;
+use PhpParser\Node\Stmt\Use_;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\BuilderFactory;
+use PhpParser\NodeTraverser;
+
+class NodePropertyReplacer extends NodeVisitorAbstract {
+    public function __construct($id, $key, $value)
+    {
+        $this->id = $id;
+        $this->key = $key;
+        $this->value = $value;
+    }
+
+    public function leaveNode(Node $node) {
+        if($node->__object_hash == $this->id) $node->{$this->key} = $this->value;
+
+        return $node;
+    }
+
+    public function afterTraverse(array $nodes) {
+        //
+    }
+    
+    public static function replace($id, $key, $value, $ast)
+    {
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor(new static($id, $key, $value));
+        return $traverser->traverse($ast);
+    }    
+}
