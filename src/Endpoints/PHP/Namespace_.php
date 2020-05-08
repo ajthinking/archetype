d01007787d6ad7f5583202e690bc0363410382be
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-08 20:12:13 +0200

    Namespace and use

---
+++ src/Endpoints/PHP/Namespace_.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace PHPFileManipulator\Endpoints\PHP;
+
+use PHPFileManipulator\Endpoints\EndpointProvider;
+use PhpParser\Node\Stmt\Namespace_ as PhpParserNamespace_;
+use PhpParser\NodeFinder;
+use PhpParser\BuilderFactory;
+
+class Namespace_ extends EndpointProvider
+{
+    public function namespace($value = null)
+    {
+        if($this->file->directive('remove')) return $this->remove();
+
+        if($value === null) return $this->get();
+
+        return $this->set($value);
+    }
+
+    protected function get()
+    {
+        $namespace = (new NodeFinder)->findFirstInstanceOf($this->ast(), PhpParserNamespace_::class);
+        return $namespace ? implode('\\', $namespace->name->parts) : null;
+    }
+
+    protected function set($newNamespace)
+    {
+        $namespace = (new NodeFinder)->findFirstInstanceOf($this->ast(), PhpParserNamespace_::class);
+        
+        if($namespace) {
+            // Modifying existing namespace
+            $namespace->name->parts = explode("\\", $newNamespace);
+        } else {
+            // Add a namespace
+            $ast = $this->file->ast();
+            array_unshift(
+                $ast,
+                (new BuilderFactory)->namespace($newNamespace)->getNode()
+            );
+
+            $this->file->ast($ast);
+        }
+        
+        return $this->file->continue();
+    }
+
+    protected function remove()
+    {
+        $namespace = (new NodeFinder)->findFirstInstanceOf($this->ast(), PhpParserNamespace_::class);
+        
+        if($namespace) {
+            $this->file->ast($namespace->stmts);
+        }
+
+        return $this->file->continue();
+    }    
+}
---
+++ src/Endpoints/PHP/Use_.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace PHPFileManipulator\Endpoints\PHP;
+
+use PHPFileManipulator\Endpoints\EndpointProvider;
+use PHPFileManipulator\Endpoints\UseStatementInserter;
+use PhpParser\Node\Stmt\Namespace_;
+use PhpParser\NodeFinder;
+use PhpParser\Node\Stmt\Use_ as PhpParserUse_;
+use PhpParser\NodeTraverser;
+
+class Use_ extends EndpointProvider
+{
+    public function use($value = null)
+    {
+        if($this->file->directive('add')) return $this->add($value);
+
+        if($value === null) return $this->get();
+
+        return $this->set($value);
+    }
+
+    protected function get()
+    {
+        return collect((new NodeFinder)->findInstanceOf($this->ast(), PhpParserUse_::class))
+            ->map(function($useStatement) {
+                return collect($useStatement->uses)->map(function($useStatement) {
+                    $base = join('\\', $useStatement->name->parts); 
+                    return $base . ($useStatement->alias ? ' as ' . $useStatement->alias : '');
+                });
+            })->flatten()->toArray();
+    }
+
+    protected function set($newUseStatements)
+    {
+        $traverser = new NodeTraverser();
+        $visitor = new UseStatementInserter($this->ast(), $newUseStatements);
+        $traverser->addVisitor($visitor);
+
+        $this->file->ast = $traverser->traverse($this->ast());
+
+        return $this->file->continue();
+    }    
+
+    protected function add($newUseStatements)
+    {
+        $namespace = (new NodeFinder)->findFirstInstanceOf($this->ast(), Namespace_::class);
+        $traverser = new NodeTraverser();
+        $visitor = new UseStatementInserter(
+            $namespace ? $this->ast()[0]->stmts : $this->ast(),
+            $newUseStatements);
+        $traverser->addVisitor($visitor);
+
+        $this->file->ast($traverser->traverse(
+            $namespace ? $this->ast()[0]->stmts : $this->ast()
+        ));
+
+        return $this->file->continue();
+    }    
+}
