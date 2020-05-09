a60dce5ec9faf67dba4f8c89a630e673f4b81274
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-09 09:51:15 +0200

    Tidy Methodnames + using AST

--- src/Endpoints/PHP/MethodNames.php
+++ src/Endpoints/PHP/MethodNames.php
@@ -3,8 +3,6 @@
 namespace PHPFileManipulator\Endpoints\PHP;
 
 use PHPFileManipulator\Endpoints\EndpointProvider;
-use PhpParser\NodeFinder;
-use PhpParser\Node\Stmt\ClassMethod;
 
 class MethodNames extends EndpointProvider
 {
@@ -15,10 +13,11 @@ class MethodNames extends EndpointProvider
 
     protected function get()
     {
-        return collect(
-            (new NodeFinder)->findInstanceOf($this->ast(), ClassMethod::class)
-        )->map(function($method) {
-            return $method->name->name;
-        })->toArray();
+        return $this->file->astQuery()
+            ->method()
+            ->name
+            ->name
+            ->get()
+            ->toArray();
     }    
 }
