e5fbc83657c96e24cc58f357c252c855b8621411
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-11 15:29:15 +0200

    PHPTemplate (Base)

---
+++ src/Endpoints/PHP/Maker/PHPTemplate.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PHPFileManipulator\Endpoints\PHP\Maker;
+
+use Illuminate\Support\Str;
+
+class PHPTemplate
+{
+    public static function make(...$args)
+    {
+        return new static(...$args);
+    }
+
+    public function in($file)
+    {
+        $this->file = $file;
+        return $this;
+    }
+
+    public function get()
+    {
+        $outputDriverClass = config('php-file-manipulator.output', \PHPFileManipulator\Drivers\FileOutput::class);
+        $outputDriver = new $outputDriverClass;
+        $outputDriver->filename = $this->filename();
+        $outputDriver->extension = $this->extension();
+        $outputDriver->relativeDir = $this->relativeDir();
+
+        $contents = $this->contents(
+            file_get_contents(static::stubPath)
+        );
+
+        $file = $this->file->fromString($contents);
+
+        $file->outputDriver($outputDriver);
+
+        return $file;           
+    }
+
+    protected function extension()
+    {
+        return 'php';
+    }
+
+    protected function contents($template)
+    {
+        return $template;
+    }
+}
