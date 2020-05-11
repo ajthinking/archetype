b02abf8d373c8b35a55d34d8b2a80a22e19fe99a
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-11 10:58:58 +0200

    testing PHP templates

---
+++ tests/Unit/Endpoints/PHP/TemplateTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PHPFileManipulator\Tests\Unit\Endpoints\PHP;
+
+use PHPFileManipulator\Tests\FileTestCase;
+use BadMethodCallException;
+use Illuminate\Support\Str;
+use UnexpectedValueException;
+
+use PHPFile;
+
+class TemplateTest extends FileTestCase
+{
+    /** @test */
+    public function it_can_make_files_with_basic_php_templates()
+    {
+        $this->assertInstanceOf(
+            \PHPFileManipulator\PHPFile::class,
+            PHPFile::make()->file('cool.php')
+        );
+
+        $this->assertInstanceOf(
+            \PHPFileManipulator\PHPFile::class,
+            PHPFile::make()->class('CoolClass')
+        );
+    }
+}
