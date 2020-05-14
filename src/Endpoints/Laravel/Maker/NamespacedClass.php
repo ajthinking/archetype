0010fd8fbdce4445f60f5c6b15c1b63b121616b7
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-14 19:14:58 +0200

    Createing a URI object in NamespacedClass

--- src/Endpoints/Laravel/Maker/NamespacedClass.php
+++ src/Endpoints/Laravel/Maker/NamespacedClass.php
@@ -3,11 +3,14 @@
 namespace PHPFileManipulator\Endpoints\Laravel\Maker;
 
 use PHPFileManipulator\Endpoints\Laravel\Maker\LaravelTemplate;
+use PHPFileManipulator\Support\URI\URIFactory;
 
 class NamepspacedClass extends LaravelTemplate
 {
     public function __construct($name)
     {
+        $uri = UriFactory::make($name);
+
         $this->filename = $name;
     }
     
