4b827d06699dac0e56212cbae62f7fa9410c7084
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-17 17:25:07 +0200

    Basic option handling for creating commands

--- src/Endpoints/Laravel/Maker/Command.php
+++ src/Endpoints/Laravel/Maker/Command.php
@@ -6,16 +6,20 @@ use PHPFileManipulator\Endpoints\Laravel\Maker\LaravelTemplate;
 
 class Command extends LaravelTemplate
 {
-    const defaults = [
-        'command' => 'command:name'
-    ];
+    const templateKeys = [
+        '{{ command }}' => 'command',
+        'dummy:command' => 'command',
+        '___command___' => 'command',
+    ];    
 
     protected $stub = 'console.stub';
 
-    public function __construct($name, $options = self::defaults)
+    public function __construct($name, $options = [])
     {
         $this->filename = $name;
-        $this->options = $options;
+        $this->namespace = 'Some\App\\Namespaze';
+        $this->class = $name;
+        $this->command = $options['command'] ?? 'command:name';
     }
     
     protected function filename()
@@ -30,18 +34,37 @@ class Command extends LaravelTemplate
 
     protected function populate($contents)
     {
-        collect($this->replacementPairs())->each(function($value, $key) use(&$contents) {
-            
-            $contents = str_replace($key, $value, $contents);
+        $contents = str_replace(['DummyNamespace'], $this->namespace, $contents);
+        $contents = str_replace(['{{ class }}', 'DummyClass', '___class___'], $this->class, $contents);
+        $contents = str_replace(['{{ command }}', 'dummy:command', '___command___'], $this->command, $contents);
 
-        });
-        
         return $contents;
     }
 
+
+
     protected function replacementPairs()
     {
-        return collect($this->options)->concat([
+        $pairs = collect($this->options)->filter(function($value, $key) {
+            return !collect($this->templateKeys)->contains($key);
+        });
+
+        $renamings = collect($this->templateKeys)->keys();
+
+        collect($this->options)->flatMap(function($value, $key) {
+
+        });
+
+
+
+
+
+
+
+
+
+
+        return $this->concat([
             '___CLASS___' => $this->filename,
             //'dummy:command' => $this->options['command'] ?? 'dummy:command',
         ])->toArray();
