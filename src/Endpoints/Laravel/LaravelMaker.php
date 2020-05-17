ff99d783662c387dfe3f5b3f9dcb4321c843166b
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-17 20:04:42 +0200

    Started simplification of Maker

--- src/Endpoints/Laravel/LaravelMaker.php
+++ src/Endpoints/Laravel/LaravelMaker.php
@@ -9,52 +9,21 @@ use PHPFileManipulator\Support\URI\UriFactory;
 
 class LaravelMaker extends Maker
 {
-    protected $publishableStubs = [
-        // ASSUMED DEFAULTS: namespace + class
-        'command'               => Maker\Command::class,
-        'model'                 => Maker\Model::class,
-
-        'controller.api'        => Unimplemented::class, // rootNamespace
-        'controller.invokable'  => Unimplemented::class, // rootNamespace
-        'controller.model.api'  => Unimplemented::class, // rootNamespace, namespacedModel, model, modelVariable
-        'controller.model'      => Unimplemented::class, // rootNamespace, namespacedModel, model, modelVariable
-        'controller.nested.api' => Unimplemented::class, // rootNamespace, namespacedModel, model, modelVariable, namespacedParentModel, parentModel, parentModelVariable
-        'controller.nested'     => Unimplemented::class, // rootNamespace, namespacedModel, model, modelVariable, namespacedParentModel, parentModel, parentModelVariable
-        'controller.plain'      => Unimplemented::class, // rootNamespace
-        'controller'            => Unimplemented::class, // rootNamespace
-        'factory'               => Unimplemented::class, // NO class // NO namespace // namespacedModel, model
-        'job.queued'            => Unimplemented::class,
-        'job'                   => Unimplemented::class,
-        'middleware'            => Unimplemented::class,
-        'migration.create'      => Unimplemented::class, // NO namespace // table
-        'migration'             => Unimplemented::class, // NO namespace
-        'migration.update'      => Unimplemented::class, // NO namespace // table
-        'model.pivot'           => Unimplemented::class,
-        
-        'policy.plain'          => Unimplemented::class, // namespacedUserModel
-        'policy'                => Unimplemented::class, // namespacedModel, namespacedUserModel, user, modelVariable, model
-        'request'               => Unimplemented::class,
-        'rule'                  => Unimplemented::class,
-        'seeder'                => Unimplemented::class, // NO NAMESPACE
-        'test'                  => Unimplemented::class,
-        'test.unit'             => Unimplemented::class,
-    ];
-
-    protected function getHandlerMethod($signature, $args)
+    public function command($name, $options = [])
     {
-        return isset($this->publishableStubs[$signature]) ? $signature : false;
-    }
+        $this->uri = UriFactory::make($name); // TODO
+        $this->filename = $name;
+        $this->namespace = 'Some\App\\Namespaze';
+        $this->class = $name;
+        $command = $options['command'] ?? 'command:name';
 
-    public function getEndpoints()
-    {
-        return collect($this->publishableStubs)->keys()->toArray();
-    }
+        $contents = file_get_contents($this->stubPath('console.stub'));
+        $contents = str_replace(['DummyNamespace', '___namespace___', '{{ namespace }}'], $this->namespace, $contents);
+        $contents = str_replace(['{{ class }}', 'DummyClass', '___class___'], $this->class, $contents);
+        $contents = str_replace(['{{ command }}', 'dummy:command', '___command___'], $command, $contents);                
 
-    public function __call($method, $args)
-    {
-        $handler = $this->publishableStubs[$method];
-
-        return $handler::make(...$args)->in($this->file)->get();        
+        return $this->file->fromString($contents)
+            ->outputDriver($this->outputDriver());        
     }
 
     public function model($name)
@@ -70,7 +39,7 @@ class LaravelMaker extends Maker
         dd($contents);
         return $this->file->fromString($contents)
             ->outputDriver($this->outputDriver());        
-    }
+    }    
 
     protected function stubPath($name)
     {
