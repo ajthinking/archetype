f494a7c0aaf8d3c752c525d3fa8ab355da26bb48
Anders Jurisoo <jurisoo@hotmail.com>
2020-05-23 11:30:18 +0200

    Test that we can pass a namespace to the maker :rocket:

--- tests/Unit/Endpoints/PHP/MakerTest.php
+++ tests/Unit/Endpoints/PHP/MakerTest.php
@@ -27,5 +27,20 @@ class MakerTest extends FileTestCase
         $this->assertEquals('app/HTTP', $output->relativeDir);
         $this->assertEquals('script', $output->filename);
         $this->assertEquals('php', $output->extension);
-    }    
+    }
+    
+    /** @test
+     * @group only*/
+    public function the_php_class_maker_accepts_a_namespaced_class()
+    {
+        $file = PHPFile::make()->class('App\Weapons\RocketLauncher');
+        
+        $output = $file->outputDriver();
+        $this->assertEquals('app/Weapons', $output->relativeDir);
+        $this->assertEquals('RocketLauncher', $output->filename);
+        $this->assertEquals('php', $output->extension);
+
+        $this->assertEquals('App\Weapons', $file->namespace());
+        $this->assertEquals('RocketLauncher', $file->className());
+    }
 }
