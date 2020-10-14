f0181bafcdfa3f113b1c30301133878bd00a9fc9
Anders Jurisoo <jurisoo@hotmail.com>
2020-10-14 14:55:46 +0200

    Cleanup2

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -49,49 +49,15 @@ class ProjectTest extends Archetype\Tests\FileTestCase
                 social_security_number hidden
                 a2 visible
             ")->build();
-        
-        $working = "/home/runner/work/archetype/archetype/host/packages/ajthinking/archetype/tests/../tests/.output/app/Models/User.php";
-        $manual1 = base_path('vendor/ajthinking/archetype/tests/.output/app/Models/User.php');
-
-        $r = shell_exec(
-            'find /home/runner/work/archetype/archetype -name User.php'
-        );
-
-        // dd($r);
-
-
-
-        // dd(
-        //     $working,
-        //     $manual1,
-        //     is_file(
-        //         $working
-        //     ),
-        //     is_file(
-        //         $manual1
-        //     ),                                    
-        // );
-
-        // dd(
-        //     is_file(
-        //         base_path('/vendor/ajthinking/archetype/tests/.output/app/Models/User.php')
-        //     ),
-        //     is_file(
-        //         base_path('packages/ajthinking/archetype/tests/.output/app/Models/User.php')
-        //     ),
-        //     is_file(
-        //         __DIR__ . '/../.output/app/Models/User.php'
-        //     ),                        
-        // );
 
         $this->assertContains(
             'social_security_number',
-            LaravelFile::load(base_path('/vendor/ajthinking/archetype/tests/.output/app/Models/User.php'))->hidden()
+            LaravelFile::load(__DIR__ . '/../.output/app/Models/User.php')->hidden()
         );
 
         $this->assertNotContains(
             'a2',
-            LaravelFile::load(base_path('/vendor/ajthinking/archetype/tests/.output/app/Models/User.php'))->hidden()
+            LaravelFile::load(__DIR__ . '/../.output/app/Models/User.php')->hidden()
         );        
     }
 }
