85e581ff280bb09ae3b0d7f0c1bccbcccf655cb8
Anders Jurisoo <jurisoo@hotmail.com>
2020-10-21 20:28:00 +0200

    Building trust with URI class

--- tests/Unit/Support/URITest.php
+++ tests/Unit/Support/URITest.php
@@ -14,9 +14,30 @@ class URITest extends Archetype\Tests\FileTestCase
         $this->assertTrue(URI::make('app/Car')->isPath());
         $this->assertTrue(URI::make('/Car')->isPath());
         
-        $this->assertFalse(URI::make('Car')->isPath());
-        $this->assertFalse(URI::make('\\Car')->isPath());
-        $this->assertFalse(URI::make('App\\Car')->isPath());
-        $this->assertFalse(URI::make('\\App\\Car')->isPath());
+        $this->assertTrue(URI::make('Car')->isName());
+        $this->assertTrue(URI::make('\\Car')->isName());
+        $this->assertTrue(URI::make('App\\Car')->isName());
+        $this->assertTrue(URI::make('\\App\\Car')->isName());
     }
+
+    /** @test */
+    public function it_can_get_resolve_namespace()
+    {
+        $namespaces = [
+            // from paths
+            'App' => URI::make('app/Cool')->namespace(),
+            'App\Models' => URI::make('app/Models/Also')->namespace(),
+            'App\Models' => URI::make('app/Models/Also.php')->namespace(),
+            'acme' => URI::make('acme/EpicTool')->namespace(), // if you expect 'Acme' - map in config
+
+            // from namespaced
+            'App\Models' => URI::make('App\Models\Moon')->namespace(),
+            'Custom' => URI::make('Custom\Star')->namespace(),
+        ];
+
+        foreach($namespaces as $expected => $actual)
+        {
+            $this->assertEquals($expected,$actual);
+        }
+    }    
 }
