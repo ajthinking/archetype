4d19bcd7cf09b7465df6b2727a17afab80fccf3c
Anders Jurisoo <jurisoo@hotmail.com>
2021-12-27 23:14:57 +0100

    Uncomment tests

--- tests/Feature/FilePathTest.php
+++ tests/Feature/FilePathTest.php
@@ -2,35 +2,35 @@
 
 use Archetype\Facades\PHPFile;
 
-// test('a_file_has_an_input_path', function() {
-// 	// relative
-// 	$file = PHPFile::load('app/Models/User.php');
-// 	$this->assertTrue(
-// 		$file->inputDriver()->absolutePath() == base_path('app/Models/User.php')
-// 	);
+test('a_file_has_an_input_path', function() {
+	// relative
+	$file = PHPFile::load('app/Models/User.php');
+	$this->assertTrue(
+		$file->inputDriver()->absolutePath() == base_path('app/Models/User.php')
+	);
 
-// 	// absolute
-// 	$path = base_path('app/Models/User.php');
-// 	$file = PHPFile::load($path);
-// 	$this->assertTrue(
-// 		$file->inputDriver()->absolutePath() == base_path('app/Models/User.php')
-// 	);
-// });
+	// absolute
+	$path = base_path('app/Models/User.php');
+	$file = PHPFile::load($path);
+	$this->assertTrue(
+		$file->inputDriver()->absolutePath() == base_path('app/Models/User.php')
+	);
+});
     
-// test('a_file_has_a_filename', function() {
-// 	// relative
-// 	$file = PHPFile::load('app/Models/User.php');
-// 	$this->assertTrue(
-// 		$file->inputDriver()->filename() == 'User'
-// 	);
+test('a_file_has_a_filename', function() {
+	// relative
+	$file = PHPFile::load('app/Models/User.php');
+	$this->assertTrue(
+		$file->inputDriver()->filename() == 'User'
+	);
 
-// 	// absolute
-// 	$path = base_path('app/Models/User.php');
-// 	$file = PHPFile::load($path);
-// 	$this->assertTrue(
-// 		$file->inputDriver()->filename() == 'User'
-// 	);
-// });
+	// absolute
+	$path = base_path('app/Models/User.php');
+	$file = PHPFile::load($path);
+	$this->assertTrue(
+		$file->inputDriver()->filename() == 'User'
+	);
+});
     
 test('files_created_with_fromString_must_be_explicitly_named', function() {
 	$file = PHPFile::fromString('<?php');
