56dcad3af750a9916ac30064c2d1dfb1f58a94e5
Anders Jurisoo <jurisoo@hotmail.com>
2022-04-19 09:08:08 +0200

    Move PHPFile resource to explicit methods

--- src/PHPFile.php
+++ src/PHPFile.php
@@ -2,7 +2,16 @@
 
 namespace Archetype;
 
+use Archetype\Endpoints\PHP\ClassConstant;
+use Archetype\Endpoints\PHP\ClassName;
+use Archetype\Endpoints\PHP\Extends_;
+use Archetype\Endpoints\PHP\Implements_;
+use Archetype\Endpoints\PHP\MethodNames;
+use Archetype\Endpoints\PHP\Namespace_;
+use Archetype\Endpoints\PHP\Property;
+use Archetype\Endpoints\PHP\Use_;
 use Archetype\Support\AST\ASTQueryBuilder;
+use Archetype\Support\Types;
 use Archetype\Traits\DelegatesAPICalls;
 use Archetype\Traits\HasDirectives;
 use Archetype\Traits\HasDirectiveHandlers;
@@ -38,21 +47,10 @@ class PHPFile
     protected $directives = [];
 
     protected const endpointProviders = [
-        // Utilities
         Endpoints\PHP\AstQuery::class,
         Endpoints\PHP\Make::class,		
         Endpoints\PHP\ReflectionProxy::class,
         Endpoints\SyntacticSweetener::class,
-
-        // Resources
-        Endpoints\PHP\ClassConstant::class,
-        Endpoints\PHP\ClassName::class,
-        Endpoints\PHP\Extends_::class,
-        Endpoints\PHP\Implements_::class,
-        Endpoints\PHP\MethodNames::class,
-        Endpoints\PHP\Namespace_::class,
-        Endpoints\PHP\Property::class,
-        Endpoints\PHP\Use_::class,
     ];
 
     public function __construct(
@@ -69,4 +67,64 @@ class PHPFile
             $this->fileQueryBuilder
         );
     }
+
+	public function property($key, $value = Types::NO_VALUE)
+	{
+		$handler = new Property($this);
+		return $handler->property($key, $value);
+	}
+
+    public function setProperty($key, $value = Types::NO_VALUE)
+    {
+		$handler = new Property($this);
+		return $handler->setProperty($key, $value);		
+    }
+	
+	public function use($value = null)
+	{
+		$handler = new Use_($this);
+		return $handler->use($value);
+	}
+
+	public function namespace(string $value = null)
+	{
+		$handler = new Namespace_($this);
+		return $handler->namespace($value);
+	}
+
+	public function methodNames()
+	{
+		$handler = new MethodNames($this);
+		return $handler->methodNames();
+	}
+	
+	public function implements($name = null)
+	{
+		$handler = new Implements_($this);
+		return $handler->implements($name);
+	}
+
+	public function extends($name = null)
+	{
+		$handler = new Extends_($this);
+		return $handler->extends($name);
+	}
+	
+	public function className($name = null)
+	{
+		$handler = new ClassName($this);
+		return $handler->className($name);
+	}
+	
+	public function classConstant($key, $value = Types::NO_VALUE)
+	{
+		$handler = new ClassConstant($this);
+		return $handler->classConstant($key, $value);
+	}
+
+	public function setClassConstant($key, $value = Types::NO_VALUE)
+	{
+		$handler = new ClassConstant($this);
+		return $handler->setClassConstant($key, $value);
+	}	
 }
