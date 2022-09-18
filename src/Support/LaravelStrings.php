0a4e7f1cdc1a56779bd59886e66f1036eb738773
Anders Jurisoo <jurisoo@hotmail.com>
2022-09-18 10:01:07 +0200

    Add types

--- src/Support/LaravelStrings.php
+++ src/Support/LaravelStrings.php
@@ -6,30 +6,14 @@ use Illuminate\Support\Str;
 
 class LaravelStrings
 {
-	public static function hasOneMethodName($target)
+	public static function hasOneMethodName($target): string
 	{
 		return Str::camel(
 			class_basename($target)
 		);
 	}
 
-	public static function hasManyMethodName($target)
-	{
-		return Str::camel(
-			Str::plural(
-				class_basename($target)
-			)
-		);
-	}
-
-	public static function belongsToMethodName($target)
-	{
-		return Str::camel(
-			class_basename($target)
-		);
-	}
-
-	public static function belongsToManyMethodName($target)
+	public static function hasManyMethodName($target): string
 	{
 		return Str::camel(
 			Str::plural(
@@ -38,14 +22,30 @@ class LaravelStrings
 		);
 	}
 
-	public static function hasOneDocBlockName($target)
+	public static function belongsToMethodName($target): string
+	{
+		return Str::camel(
+			class_basename($target)
+		);
+	}
+
+	public static function belongsToManyMethodName($target): string
+	{
+		return Str::camel(
+			Str::plural(
+				class_basename($target)
+			)
+		);
+	}
+
+	public static function hasOneDocBlockName($target): string
 	{
 		return Str::studly(
 			class_basename($target)
 		);
 	}
 
-	public static function hasManyDocBlockName($target)
+	public static function hasManyDocBlockName($target): string
 	{
 		return Str::studly(
 			Str::plural(
@@ -54,14 +54,14 @@ class LaravelStrings
 		);
 	}
 
-	public static function belongsToDocBlockName($target)
+	public static function belongsToDocBlockName($target): string
 	{
 		return Str::studly(
 			class_basename($target)
 		);
 	}
 
-	public static function belongsToManyDocBlockName($target)
+	public static function belongsToManyDocBlockName($target): string
 	{
 		return Str::studly(
 			Str::plural(
